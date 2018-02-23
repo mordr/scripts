1eada210a666911c4d13e8900ac021125461ba6d
Ragib Morshed <mordr@users.noreply.github.com>
2018-02-23 15:34:27 -0800

    add include guard

--- error.sh
+++ error.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Prevent get sourced more than once
+[ -n "$ERROR_SCRIPT" ] && return || readonly ERROR_SCRIPT=1
+
 # Usage: error "Received interrupt, exiting"
 error () {
     echo "$(tput setaf 1)[ERROR] $@$(tput sgr0)" >&2
