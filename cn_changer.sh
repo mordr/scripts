d071dc504b8e0b36ce09e12ba1c43635854bd916
Ragib Morshed <mordr@users.noreply.github.com>
2019-01-25 15:48:26 -0800

    update to change multiple

--- cn_changer.sh
+++ cn_changer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo 'Changing computer name..'
-openssl rand -hex 13 | base64 | sudo scutil --set ComputerName
-COMPUTER_NAME=$(sudo scutil --get ComputerName)
-echo "New computer name: ${COMPUTER_NAME}"
+COMPUTER_NAME=$(openssl rand -hex 13 | base64)
+sudo scutil --set ComputerName ${COMPUTER_NAME}
+sudo scutil --set LocalHostName ${COMPUTER_NAME}
+echo "New identifier: ${COMPUTER_NAME}"
 echo 'Done'
